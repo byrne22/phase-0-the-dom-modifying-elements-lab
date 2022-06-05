bba3f7c34d1ae162235e4ed016600b1c5a367fb6
byrne22 <byron.odhiambo@student.moringaschool.com>
2022-06-05 16:35:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+  
+// Remove the <main> element with id 'main'
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+//'newHeader' variable that points to node 'h1#victory'
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+ 
+//Insert message in the h1 element
+newHeader.textContent += "Byron is the champion";
